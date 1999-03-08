af7ba7bff13583f38e45ec8bc9741cbe5ad8085c
Arturo Espinosa <unammx@src.gnome.org>
1999-03-08 19:30:56 +0000

    Jesus, so many files I forgot to commit

---
+++ bonobo/bonobo-item-container.c
@@ -0,0 +1,162 @@
+/**
+ * GNOME view object
+ *
+ * Author:
+ *   Miguel de Icaza (miguel@kernel.org)
+ */
+#include <config.h>
+#include <gtk/gtksignal.h>
+#include <gtk/gtkmarshal.h>
+#include <gtk/gtkwidget.h>
+#include <bonobo/gnome-main.h>
+#include <bonobo/gnome-object.h>
+#include <bonobo/gnome-container.h>
+
+static GnomeObjectClass *gnome_container_parent_class;
+
+POA_GNOME_Container__epv gnome_container_epv = {
+	NULL,
+};
+	
+POA_GNOME_Container__vepv gnome_container_vepv = {
+	&gnome_object_base_epv,
+	&gnome_object_epv,
+	&gnome_container_epv
+};
+
+static CORBA_Object
+create_gnome_container (GnomeObject *object)
+{
+	POA_GNOME_Container *servant;
+	CORBA_Object o;
+	
+	servant = g_new0 (POA_GNOME_Container, 1);
+	servant->vepv = &gnome_container_vepv;
+
+	POA_GNOME_Container__init ((PortableServer_Servant) servant, &object->ev);
+	if (object->ev._major != CORBA_NO_EXCEPTION){
+		g_free (servant);
+		return CORBA_OBJECT_NIL;
+	}
+
+	CORBA_free (PortableServer_POA_activate_object (
+		bonobo_poa (), servant, &object->ev));
+
+	o = PortableServer_POA_servant_to_reference (
+		bonobo_poa(), servant, &object->ev);
+
+	if (o){
+		gnome_object_bind_to_servant (object, servant);
+		return o;
+	} else
+		return CORBA_OBJECT_NIL;
+}
+
+GnomeMoniker *
+gnome_container_get_moniker (GnomeContainer *container)
+{
+	g_error ("Implement me");
+	return NULL;
+}
+
+static void
+gnome_container_destroy (GtkObject *object)
+{
+	GnomeContainer *container = GNOME_CONTAINER (object);
+
+	GTK_OBJECT_CLASS (gnome_container_parent_class)->destroy (object);
+}
+
+static void
+gnome_container_class_init (GnomeContainerClass *container_class)
+{
+	GtkObjectClass *object_class = (GtkObjectClass *) container_class;
+
+	gnome_container_parent_class = gtk_type_class (gtk_object_get_type ());
+
+	object_class->destroy = gnome_container_destroy;
+}
+
+static void
+gnome_container_init (GnomeContainer *container)
+{
+}
+
+GnomeObject *
+gnome_container_construct (GnomeContainer  *container,
+			   GNOME_Container corba_container)
+{
+	g_return_val_if_fail (container != NULL, NULL);
+	g_return_val_if_fail (GNOME_IS_CONTAINER (container), NULL);
+	g_return_val_if_fail (corba_container != CORBA_OBJECT_NIL, NULL);
+	
+	gnome_object_construct (GNOME_OBJECT (container), (CORBA_Object) corba_container);
+	GNOME_OBJECT (container)->object = corba_container;
+
+	return GNOME_OBJECT (container);
+}
+
+GtkType
+gnome_container_get_type (void)
+{
+	static GtkType type = 0;
+
+	if (!type){
+		GtkTypeInfo info = {
+			"IDL:GNOME/Container:1.0",
+			sizeof (GnomeContainer),
+			sizeof (GnomeContainerClass),
+			(GtkClassInitFunc) gnome_container_class_init,
+			(GtkObjectInitFunc) gnome_container_init,
+			NULL, /* reserved 1 */
+			NULL, /* reserved 2 */
+			(GtkClassInitFunc) NULL
+		};
+
+		type = gtk_type_unique (gnome_object_get_type (), &info);
+	}
+
+	return type;
+}
+
+GnomeObject *
+gnome_container_new (void)
+{
+	GnomeContainer *container;
+	GNOME_Container corba_container;
+
+	container = gtk_type_new (gnome_container_get_type ());
+	corba_container = create_gnome_container (GNOME_OBJECT (container));
+
+	if (corba_container == CORBA_OBJECT_NIL){
+		gtk_object_destroy (GTK_OBJECT (container));
+		return NULL;
+	}
+	
+	return gnome_container_construct (container, corba_container);
+}
+
+void
+gnome_container_add (GnomeContainer *container, GnomeObject *client_site)
+{
+	g_return_if_fail (container != NULL);
+	g_return_if_fail (client_site != NULL);
+	g_return_if_fail (GNOME_IS_CONTAINER (container));
+	g_return_if_fail (GNOME_IS_OBJECT (client_site));
+
+	gtk_object_ref (GTK_OBJECT (client_site));
+	container->client_sites = g_list_prepend (container->client_sites, client_site);
+}
+
+void
+gnome_container_remove (GnomeContainer *container, GnomeObject *client_site)
+{
+	g_return_if_fail (container != NULL);
+	g_return_if_fail (client_site != NULL);
+	g_return_if_fail (GNOME_IS_CONTAINER (container));
+	g_return_if_fail (GNOME_IS_OBJECT (client_site));
+
+	container->client_sites = g_list_remove (container->client_sites, client_site);
+	gtk_object_unref (GTK_OBJECT (client_site));
+}
+
