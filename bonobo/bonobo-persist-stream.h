cd81a34221b2d2058f550b0dcf78f0d4d298f20e
Michael Meeks <michael@helixcode.com>
2000-05-02 20:48:56 +0000

    Bonobo selector now shows all the components =)
    2000-05-02  Michael Meeks  <michael@helixcode.com>
    
    	* components/text-plain/bonobo-text-plain.c (init_server_factory):
    	use the right orb.
    
    	* bonobo/bonobo-selector.c (get_filtered_objects): fix serious bug
    	in sorting.
    
    	* bonobo/bonobo-object-directory-oaf.c (od_get_server_list): clean.
    
    	* idl/bonobo-storage.idl: fix docs.
    
    Also:
    
    2000-05-02  Michael Meeks  <michael@helixcode.com>
    
    	* minefield.c (minefield_set_at, minefield_get_at): implement.
    
    	* bonobo-application-x-mines.c (embeddable_factory): bind
    	PersistStream. (save_board, load_board): Implement.

--- bonobo/bonobo-persist-stream.h
+++ bonobo/bonobo-persist-stream.h
@@ -12,12 +12,15 @@ BEGIN_GNOME_DECLS
 #define BONOBO_IS_PERSIST_STREAM(o)       (GTK_CHECK_TYPE ((o), BONOBO_PERSIST_STREAM_TYPE))
 #define BONOBO_IS_PERSIST_STREAM_CLASS(k) (GTK_CHECK_CLASS_TYPE ((k), BONOBO_PERSIST_STREAM_TYPE))
 
-typedef struct _BonoboPersistStream BonoboPersistStream;
+typedef struct _BonoboPersistStream        BonoboPersistStream;
 typedef struct _BonoboPersistStreamPrivate BonoboPersistStreamPrivate;
 
 
-typedef int (*BonoboPersistStreamIOFn)(BonoboPersistStream *ps, const Bonobo_Stream stream, void *closure);
-typedef CORBA_long (*BonoboPersistStreamMaxFn)(BonoboPersistStream *ps, void *closure);
+typedef int        (*BonoboPersistStreamIOFn)  (BonoboPersistStream *ps,
+						const Bonobo_Stream  stream,
+						void                *closure);
+typedef CORBA_long (*BonoboPersistStreamMaxFn) (BonoboPersistStream *ps,
+						void                *closure);
 
 struct _BonoboPersistStream {
 	BonoboPersist persist;
@@ -43,23 +46,25 @@ typedef struct {
 	/*
 	 * methods
 	 */
-	int        (*load)(BonoboPersistStream *ps, Bonobo_Stream stream);
-	int        (*save)(BonoboPersistStream *ps, Bonobo_Stream stream);
+	int        (*load)         (BonoboPersistStream *ps,
+				    Bonobo_Stream        stream);
+	int        (*save)         (BonoboPersistStream *ps,
+				    Bonobo_Stream        stream);
 	CORBA_long (*get_size_max) (BonoboPersistStream *ps);
 } BonoboPersistStreamClass;
 
-GtkType             bonobo_persist_stream_get_type  (void);
-void                bonobo_persist_stream_set_dirty (BonoboPersistStream *ps,
-						    gboolean dirty);
+GtkType              bonobo_persist_stream_get_type  (void);
+void                 bonobo_persist_stream_set_dirty (BonoboPersistStream *ps,
+						      gboolean dirty);
 
 BonoboPersistStream *bonobo_persist_stream_new       (BonoboPersistStreamIOFn load_fn,
-						    BonoboPersistStreamIOFn save_fn,
-						    void *closure);
+						      BonoboPersistStreamIOFn save_fn,
+						      void *closure);
 BonoboPersistStream *bonobo_persist_stream_construct (BonoboPersistStream *ps,
-						    Bonobo_PersistStream corba_ps,
-						    BonoboPersistStreamIOFn load_fn,
-						    BonoboPersistStreamIOFn save_fn,
-						    void *closure);
+						      Bonobo_PersistStream corba_ps,
+						      BonoboPersistStreamIOFn load_fn,
+						      BonoboPersistStreamIOFn save_fn,
+						      void *closure);
 
 POA_Bonobo_PersistStream__epv *bonobo_persist_stream_get_epv (void);
 
