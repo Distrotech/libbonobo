6de1b51cf6d5d66314ca72b6b0208b57f8d37d4e
Michael Meeks <michael@helixcode.com>
2000-11-14 15:24:31 +0000

    remove appended '/'
    2000-11-14  Michael Meeks  <michael@helixcode.com>
    
    	* tests/Makefile.am (BONOBO_PLUGIN_PATH): remove appended '/'
    
    	* bonobo/bonobo-storage-plugin.c (bonobo_storage_load_plugins):
    	use g_concat_dir_and_file.

--- bonobo/bonobo-storage-plugin.c
+++ bonobo/bonobo-storage-plugin.c
@@ -13,6 +13,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <libgnome/gnome-defs.h>
+#include <libgnome/gnome-util.h>
 #include "bonobo-storage-plugin.h"
 
 #define PLUGIN_PREFIX "libstorage_"
@@ -89,8 +91,7 @@ bonobo_storage_load_plugins (void)
 			    strncmp (de->d_name, PLUGIN_PREFIX, 
 				     strlen (PLUGIN_PREFIX)) == 0 &&
 			    strncmp (de->d_name + len - 3, ".so", 3) == 0) {
-				plugin_name=g_strconcat (path, "/", 
-							 de->d_name, NULL);
+				plugin_name = g_concat_dir_and_file (path, de->d_name);
 				plugin_load (plugin_name);
 				g_free (plugin_name);
 			}
