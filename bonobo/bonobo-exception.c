7278c0ca4797f1177a642c3886d2ba2c1ad87549
Jason Leach <jasonleach@usa.net>
2001-01-13 17:39:29 +0000

    replaced an #include <gnome.h> with necessary headers to trim
    2001-01-13  Jason Leach  <jasonleach@usa.net>
    
    	* bonobo/bonobo-exception.c: replaced an #include <gnome.h>
              with necessary headers to trim dependencies.
    
    	* bonobo/bonobo-object-directory.c: Ditto.
    	* bonobo/bonobo-selector.c: Ditto.
    	* bonobo/bonobo-selector-widget.c: Ditto.
    	* bonobo/bonobo-win.c: Ditto.
    
    	* bonobo/bonobo-main.h: removed unnecessary #include
    	  <libgnome/gnome-defs.h>
    
    	* bonobo/bonobo-main.c: removed unnecessary #include <gnome.h>

--- bonobo/bonobo-exception.c
+++ bonobo/bonobo-exception.c
@@ -8,7 +8,9 @@
  */
 
 #include <config.h>
-#include <gnome.h>
+#include <glib.h>
+#include <libgnome/gnome-defs.h>
+#include <libgnome/gnome-i18n.h>
 #include <bonobo/bonobo-exception.h>
 
 typedef enum {
