d91cd6fa49b5b6a54dea42740731e778236e27be
Michael Meeks <michael@ximian.com>
2001-04-20 19:50:44 +0000

    add bonobo-config-database.[ch]
    2001-04-20  Michael Meeks  <michael@ximian.com>
    
    	* bonobo/Makefile.am: add bonobo-config-database.[ch]
    
    	* bonobo/bonobo-config-database.[ch]: add from bonobo-conf
    
    	* idl/Makefile.am: add Bonobo_ConfigDatabase.idl
    
    	* idl/Bonobo_ConfigDatabase.idl: add.

--- bonobo/bonobo-config-database.h
+++ bonobo/bonobo-config-database.h
@@ -1,85 +1,17 @@
 /**
- * bonobo-config-database.h: config database object implementation.
+ * bonobo-config-database.h: config database client methods.
  *
  * Author:
  *   Dietmar Maurer  (dietmar@ximian.com)
  *
- * Copyright 2000 Ximian, Inc.
+ * Copyright 2001 Ximian, Inc.
  */
 #ifndef __BONOBO_CONFIG_DATABASE_H__
 #define __BONOBO_CONFIG_DATABASE_H__
 
-#include <bonobo/bonobo-xobject.h>
-#include <bonobo-conf/Bonobo_Config.h>
+#include <bonobo/Bonobo.h>
 
-BEGIN_GNOME_DECLS
-
-#define BONOBO_CONFIG_DATABASE_TYPE        (bonobo_config_database_get_type ())
-#define BONOBO_CONFIG_DATABASE(o)	   (GTK_CHECK_CAST ((o), BONOBO_CONFIG_DATABASE_TYPE, BonoboConfigDatabase))
-#define BONOBO_CONFIG_DATABASE_CLASS(k)    (GTK_CHECK_CLASS_CAST((k), BONOBO_CONFIG_DATABASE_TYPE, BonoboConfigDatabaseClass))
-#define BONOBO_IS_CONFIG_DATABASE(o)	   (GTK_CHECK_TYPE ((o), BONOBO_CONFIG_DATABASE_TYPE))
-#define BONOBO_IS_CONFIG_DATABASE_CLASS(k) (GTK_CHECK_CLASS_TYPE ((k), BONOBO_CONFIG_DATABASE_TYPE))
-
-typedef struct _BonoboConfigDatabasePrivate BonoboConfigDatabasePrivate;
-typedef struct _BonoboConfigDatabase        BonoboConfigDatabase;
-
-struct _BonoboConfigDatabase {
-	BonoboXObject base;
-
-	gboolean writeable;
-
-	BonoboConfigDatabasePrivate *priv;
-};
-
-typedef struct {
-	BonoboXObjectClass  parent_class;
-
-	POA_Bonobo_ConfigDatabase__epv epv;
-
-        /*
-         * virtual methods
-         */
-
-	CORBA_any      *(*get_value)    (BonoboConfigDatabase *db,
-					 const CORBA_char     *key, 
-					 const CORBA_char     *locale,
-					 CORBA_Environment    *ev);
-
-	void            (*set_value)    (BonoboConfigDatabase *db,
-					 const CORBA_char     *key, 
-					 const CORBA_any      *value,
-					 CORBA_Environment    *ev);
-
-	Bonobo_KeyList *(*list_dirs)    (BonoboConfigDatabase *db,
-					 const CORBA_char     *dir,
-					 CORBA_Environment    *ev);
-
-	Bonobo_KeyList *(*list_keys)    (BonoboConfigDatabase *db,
-					 const CORBA_char     *dir,
-					 CORBA_Environment    *ev);
-
-	CORBA_boolean   (*dir_exists)   (BonoboConfigDatabase *db,
-					 const CORBA_char     *dir,
-					 CORBA_Environment    *ev);
-
-	void            (*remove_value) (BonoboConfigDatabase *db,
-					 const CORBA_char     *key, 
-					 CORBA_Environment    *ev);
-
-	void            (*remove_dir)   (BonoboConfigDatabase *db,
-					 const CORBA_char     *dir, 
-					 CORBA_Environment    *ev);
-
-	void            (*sync)         (BonoboConfigDatabase *db, 
-					 CORBA_Environment    *ev);
-
-} BonoboConfigDatabaseClass;
-
-
-GtkType		      
-bonobo_config_database_get_type  (void);
-
-/* some handy utility functions */
+G_BEGIN_DECLS
 
 gchar *
 bonobo_config_get_string               (Bonobo_ConfigDatabase  db,
@@ -219,6 +151,6 @@ bonobo_config_set_value                (Bonobo_ConfigDatabase  db,
 					CORBA_any             *value,
 					CORBA_Environment     *opt_ev);
 
-END_GNOME_DECLS
+G_END_DECLS
 
 #endif /* ! __BONOBO_CONFIG_DATABASE_H__ */
