374642eea76488f54c406f020929eae6f5dcb478
Michael Meeks <michael@ximian.com>
2001-05-02 23:03:36 +0000

    add test-object
    2001-05-02  Michael Meeks  <michael@ximian.com>
    
    	* tests/Makefile.am: add test-object
    
    	* tests/test-object.c: add from HEAD bonobo test-xobject

--- tests/test-object.c
+++ tests/test-object.c
@@ -1,30 +1,23 @@
 #include <config.h>
-#include <gnome.h>
-#include <bonobo.h>
-#include <bonobo/bonobo-property-bag-xml.h>
+#include <stdlib.h>
+#include <libbonobo.h>
 
 int
 main (int argc, char *argv [])
 {
-	BonoboXObject *xobject;
-
-	CORBA_ORB    orb;
+	BonoboObject *object;
 
 	free (malloc (8));
 
-	gtk_type_init ();
-
-	orb = oaf_init (argc, argv);
-	
-	if (bonobo_init (orb, NULL, NULL) == FALSE)
+	if (bonobo_init (&argc, argv) == FALSE)
 		g_error ("Can not bonobo_init");
 
-	xobject = BONOBO_X_OBJECT (gtk_type_new (bonobo_moniker_get_type ()));
+	object = BONOBO_OBJECT (g_object_new (bonobo_moniker_get_type (), NULL));
 	
-	g_assert (bonobo_x_object (xobject) == xobject);
-	g_assert (bonobo_x_object (&xobject->servant) == xobject);
+	g_assert (bonobo_object (object) == object);
+	g_assert (bonobo_object (&object->servant) == object);
 
-	bonobo_object_unref (BONOBO_OBJECT (xobject));
+	bonobo_object_unref (BONOBO_OBJECT (object));
 
 	return 0;
 }
