e4ac755026543fae4f33ca9d7122c8cef36fd266
Michael Meeks <michael@ximian.com>
2003-12-09 14:36:41 +0000

    switch to using environ instead - with our own extern for Solaris.
    2003-12-09  Michael Meeks  <michael@ximian.com>
    
    	* bonobo-activation/bonobo-activation-client.c
    	(bonobo_activation_register_client): switch to using
    	environ instead - with our own extern for Solaris.

--- bonobo-activation/bonobo-activation-client.c
+++ bonobo-activation/bonobo-activation-client.c
@@ -197,6 +197,8 @@ get_lang_list (void)
         return result;
 }
 
+extern char **environ;
+
 void
 bonobo_activation_register_client (Bonobo_ActivationContext context,
                                    CORBA_Environment       *ev)
@@ -217,7 +219,8 @@ bonobo_activation_register_client (Bonobo_ActivationContext context,
                 bonobo_activation_hostname_get ());
 
         /* send environment to activation server */
-        client_env._buffer = __environ;
+        client_env._buffer = environ;
+
         if (client_env._buffer) {
                 for (i = 0; client_env._buffer[i]; ++i);
                 client_env._length = i;
