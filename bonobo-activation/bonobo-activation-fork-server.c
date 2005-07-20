79c8521bb23cfdf45ac932dfdbab845886065b1e
Shreyas Srinivasan <sshreyas@novell.com>
2005-07-20 07:02:15 +0000

    Fix #310152, Resolve environ to NSGetEnviron on a mac os x.
    2005-07-20  Shreyas Srinivasan  <sshreyas@novell.com>
    
    	* bonobo/bonobo-activation-fork-server.c: Fix #310152, Resolve environ to
    	NSGetEnviron on a mac os x.

--- bonobo-activation/bonobo-activation-fork-server.c
+++ bonobo-activation/bonobo-activation-fork-server.c
@@ -228,6 +228,12 @@ bonobo_activation_server_by_forking (
         GSource *source;
         GMainContext *context;
         char **newenv = NULL;
+
+#if defined(__APPLE__) && defined(HAVE_NSGETENVIRON) && defined(HAVE_CRT_EXTERNS_H)
+# include <crt_externs.h>
+# define environ (*_NSGetEnviron())
+#endif
+
 #ifndef G_OS_WIN32
         extern char **environ;
 #endif
